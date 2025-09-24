961449edaa057b5093630d011c81e7ba881219f9
eli <eliadadearaujolinhares@gmail.com>
2025-09-24 20:26:05 -0300

    Add files via upload

---
+++ atividade06.py
@@ -0,0 +1,10 @@
+# Atividade 6 – Conversão de Tipos
+# Peça ao usuário para digitar dois números.
+# Converta para int e mostre a soma dos dois valores.
+
+numero1 = input("digite seu primeiro número: ")
+numero2 = input("digite seu segundo número: ")
+numero_inteiro1 = int(numero1)
+numero_inteiro2 = int(numero2)
+soma = numero_inteiro1 + numero_inteiro2 
+print(f'A soma dos números é: {soma}')
